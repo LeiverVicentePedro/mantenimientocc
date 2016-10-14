88ff9164d3184cce6fc8e64d437d91671db203c0
Jerusalen <Jerusalen@10.168.97.79>
2016-10-14 09:27:57 -0500

    En clase AreaBean,AreaDAO,Area se crearon los metodos para registrar, mostrar modificar y en proceso metodo eliminar

---
+++ src/java/mx/edu/itoaxaca/mantenimientocc/modelo/Area.java
@@ -0,0 +1,38 @@
+
+package mx.edu.itoaxaca.mantenimientocc.modelo;
+
+
+public class Area {
+    
+    private int idarea;
+    private String nombre_area;
+    private Boolean estatus;
+
+    public int getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(int idarea) {
+        this.idarea = idarea;
+    }
+
+    public String getNombre_area() {
+        return nombre_area;
+    }
+
+    public void setNombre_area(String nombre_area) {
+        this.nombre_area = nombre_area;
+    }
+
+    public Boolean getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(Boolean estatus) {
+        this.estatus = estatus;
+    }
+    
+    
+    
+    
+}
