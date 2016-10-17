6f9fb6c0548a2dfe51410dbacfe0a88d236ce0bd
Jerusalen <Jerusalen@10.168.97.79>
2016-10-17 15:19:05 -0500

    Departamento1.4

--- src/java/mx/edu/itoaxaca/mantenimientocc/dao/DepartamentoDAO.java
+++ src/java/mx/edu/itoaxaca/mantenimientocc/dao/DepartamentoDAO.java
@@ -5,10 +5,35 @@
  */
 package mx.edu.itoaxaca.mantenimientocc.dao;
 
+import java.sql.PreparedStatement;
+import mx.edu.itoaxaca.mantenimientocc.conexion.Conexion;
+import mx.edu.itoaxaca.mantenimientocc.modelo.Departamento;
+
+
 /**
  *
  * @author Jerusalen
  */
-public class DepartamentoDAO {
+public class DepartamentoDAO extends Conexion{
+    
+    public void registrarDepartamento(Departamento departamentoregistra) throws Exception{
+        try{
+            this.Conectar();
+            PreparedStatement consulta= this.getConexion().prepareStatement("INSERT INTO departamento (clave_departamento,nombre_departamento,id_area,estatus) values(?,?,?,?)");
+            consulta.setString(1, departamentoregistra.getClaveDepartamento());
+            consulta.setString(2,departamentoregistra.getNombreDepartamento());
+            consulta.setInt(3,departamentoregistra.getArea().getIdarea());
+            consulta.setBoolean(4,departamentoregistra.getEstatus());
+            
+            consulta.executeUpdate();
+        }
+        catch(Exception e){
+           System.out.println("error en DAO"); 
+        }
+        finally{
+           this.Cerrar();
+        }
+    }
+    
     
 }
