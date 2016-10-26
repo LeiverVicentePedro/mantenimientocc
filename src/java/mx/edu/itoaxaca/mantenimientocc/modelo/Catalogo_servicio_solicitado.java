f003e893e6330b9a82c608368b315908efbc7c0c
Jerusalen <Jerusalen@10.168.97.79>
2016-10-26 16:23:19 -0500

    pruebas en catalogo

---
+++ src/java/mx/edu/itoaxaca/mantenimientocc/modelo/Catalogo_servicio_solicitado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.itoaxaca.mantenimientocc.modelo;
+
+/**
+ *
+ * @author Jerusalen
+ */
+public class Catalogo_servicio_solicitado {
+    
+    private int idcatalogo_servicio_solicitado;
+    private String servicio_solicitado;
+
+    public int getIdcatalogo_servicio_solicitado() {
+        return idcatalogo_servicio_solicitado;
+    }
+
+    public void setIdcatalogo_servicio_solicitado(int idcatalogo_servicio_solicitado) {
+        this.idcatalogo_servicio_solicitado = idcatalogo_servicio_solicitado;
+    }
+
+    public String getServicio_solicitado() {
+        return servicio_solicitado;
+    }
+
+    public void setServicio_solicitado(String servicio_solicitado) {
+        this.servicio_solicitado = servicio_solicitado;
+    }
+    
+    
+    
+}
