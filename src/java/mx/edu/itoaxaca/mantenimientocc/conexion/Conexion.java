6c92c0dedf4c027dc7ae7549b8925b0f37bca6f9
leiver <leiver@10.168.71.214>
2017-06-08 11:11:01 -0500

    cambios en archivo de conexion se eliminaron los datos duplicados y se limpio los comentarios.

--- src/java/mx/edu/itoaxaca/mantenimientocc/conexion/Conexion.java
+++ src/java/mx/edu/itoaxaca/mantenimientocc/conexion/Conexion.java
@@ -28,14 +28,8 @@ public class Conexion {
     public void Conectar() throws Exception{
         try{
      Class.forName("com.mysql.jdbc.Driver");
-    /* String servidor = "jdbc:mysql://sql37.hostinger.mx:3306/u135348475_mant";
-            String usuarioDB="u135348475_root";
-            String passwordDB="administrador"; 
-      conexion = DriverManager.getConnection("jdbc:mysql://mysql140309-simapreco.j.layershift.co.uk/mantenimientobd?user=root&password=TFSbhb72636");*/
-      conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/mantenimientobd?user=root&password=sistemaTec01");
-       //conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/mantenimientobd?user=root&password=admin");
-     
-   //  conexion= DriverManager.getConnection(servidor,usuarioDB,passwordDB);
+      //conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/mantenimientobd?user=root&password=sistemaTec01");
+       conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/mantenimientobd?user=root&password=admin");
         }
      catch(Exception ex){
         System.out.println("Error de Conexion" +ex+" ==============================");
